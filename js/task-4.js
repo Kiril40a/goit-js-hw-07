d0cdc88731e2c5eba87fa618a8ab364ee17ec988
Kiril40a <kirilsoroka88@gmail.com>
2025-04-06 17:01:31 +0300

    task 4 done

--- js/task-4.js
+++ js/task-4.js
@@ -1,1 +1,19 @@
+document.querySelector('.login-form').addEventListener('submit', function (event) {  
+    event.preventDefault();
 
+    const form = event.target;
+    const formData = {};  
+ 
+    for (let element of form.elements) {  
+        if (element.tagName.toLowerCase() === 'button') continue;  
+
+        if (element.value.trim() === '') {  
+            alert('All form fields must be filled in');  
+            return;
+        }  
+        formData[element.name] = element.value.trim();   
+    }  
+
+    console.log(formData);  
+    form.reset();  
+});  
